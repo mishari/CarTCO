483c48f5efb25ba26e6b1240b02ebfcc27f65198
Mishari Muqbil <mishari@mishari.net>
2014-12-11 11:37:24 +0700

    Added licensing information

--- calculator.js
+++ calculator.js
@@ -1,3 +1,21 @@
+/*
+    CarTCO calculates the Total Cost of Ownership of buying a car vs other
+    transportation options.
+    Copyright (C) 2014  Mishari Muqbil
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 angular.module('todoApp', [])
 .controller('CostCalculatorController', ['$scope', function($scope) {
   /* Average speed in BKK from
